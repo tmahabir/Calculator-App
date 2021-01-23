92c9a40598d0fe4ff649d384638759c6a9d7b18d
Tenzin Mahabir <tmahabir10@gmail.com>
2021-01-23 12:28:01 -0500

    Initial Commit

---
+++ Shared/Calculator_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_AppApp.swift
+//  Shared
+//
+//  Created by Tenzin Mahabir on 2021-01-23.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
